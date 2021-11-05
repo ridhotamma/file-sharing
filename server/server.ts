06546dd0e3890b374a21fc5175bc69fe691404c9
Ridho Tamma <ridhotamma@outlook.com>
2021-11-05 17:19:46 +0700

    fixing typos

--- server/server.ts
+++ server/server.ts
@@ -1,18 +1,32 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import connectDB from "./config/database"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/database";
+import fileRoute from "./routes/files";
 
-const app = express()
-dotenv.config()
+import { v2 as cloudinary } from "cloudinary";
 
-// database connection
-connectDB()
+const app = express();
+dotenv.config();
 
-//middleware
-app.use(cors())
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_API_CLOUD,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-const port = process.env.PORT 
+connectDB();
 
-app.listen(port, () => console.log(`server is running on port ${port}`))
+app.use(cors());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
+app.use("/api/files", fileRoute);
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log(`Server is listening on PORT ${PORT}`));
